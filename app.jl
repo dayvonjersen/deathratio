974586ab2aeb36afc18ccde1dae0efddedc795d7
tso <tso@teknik.io>
2015-07-03 02:52:31 -0400

    More setup

--- app.jl
+++ app.jl
@@ -1,18 +1,32 @@
 #!/usr/bin/julia
 using HttpCommon
 using HttpServer
+using HTTPClient
+using JSON
+using Mustache
+
+function github_api(url::String)
+    return JSON.parse(takebuf_string(get(string("https://api.github.com/",url))))
+end
+
+function view(tpl::String, data::Dict)
+    return Mustache.render(Mustache.template_from_file(string("./tpl/", tpl, ".tpl")), data)
+end
 
 function mainhandler(req::Request,res::Response)
     if ismatch(r"^/static/*",req.resource)
-        res2 = FileResponse(string("/srv/deathratio",req.resource))
-        res.status = res2.status
+        res2        = FileResponse(string(".",req.resource))
+        res.status  = res2.status
         res.headers = res2.headers
-        res.data = res2.data
+        res.data    = res2.data
+    elseif ismatch(r"^/test/?", req.resource)
+        res.status = 200
+        res.data   = view("test", Dict({"something"=>"else"}))
     else
         res.status = 404
-        res.data = "Requested resource not found."
+        res.data   = "Requested resource not found."
     end
-    res
+    return res
 end
 
 server = Server(mainhandler)
---
+++ tpl/test.tpl
@@ -0,0 +1,1 @@
+<h1>do it or {{ something }}</h1>
