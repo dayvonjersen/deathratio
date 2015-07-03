6866f5f25a784f68c04a5fa7b044003040990ad5
tso <tso@teknik.io>
2015-07-03 06:50:14 -0400

    . . .

--- app.jl
+++ app.jl
@@ -2,15 +2,22 @@
 using HttpCommon
 using HttpServer
 using HTTPClient
+using HTTPClient.HTTPC
 using JSON
 using Mustache
 using Morsel
 using Meddle
 
 function github_api(url::String)
-    jsan = HTTPClient.get(string("https://api.github.com/",url))
+    jsan = HTTPClient.get(string("https://api.github.com/",url),RequestOptions(headers=[("User-Agent","Web/4.0 [julia]")]))
 #    return JSON.parse(takebuf_string(jsan.body))
-    takebuf_string(jsan.body)
+    try
+        plsrespond = takebuf_string(jsan.body)
+        r = JSON.parse(plsrespond)
+        return string("full name: ", r["full_name"], "\ndescription: ", r["description"])
+    catch
+        return plsrespond
+    end
 end
 
 function view(tpl::String, data::Dict)
@@ -31,7 +38,7 @@ end
 
 route(app, POST, "/test") do req, res
     if !haskey(req.state[:data],"repo") || !ismatch(r"^\w+\/\w+$",req.state[:data]["repo"])
-        return view("test", Dict({"invalid"=>true}))
+        return view("test", Dict{String,Any}("invalid"=>true))
     end
     res.headers["Content-Type"] = "text/plain"
     github_api(string("repos/",req.state[:data]["repo"]))
