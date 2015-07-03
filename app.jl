e151000e11acd48da4dc535f99eeea71d0a4d269
tso <tso@teknik.io>
2015-07-03 04:59:05 -0400

    ...

--- app.jl
+++ app.jl
@@ -8,31 +8,14 @@ using Morsel
 using Meddle
 
 function github_api(url::String)
-    return JSON.parse(takebuf_string(get(string("https://api.github.com/",url))))
+    jsan = HTTPClient.get(string("https://api.github.com/",url))
+#    return JSON.parse(takebuf_string(jsan.body))
+    takebuf_string(jsan.body)
 end
 
 function view(tpl::String, data::Dict)
     return Mustache.render(Mustache.template_from_file(string("./tpl/", tpl, ".tpl")), data)
 end
-
-function mainhandler(req::Request,res::Response)
-    if ismatch(r"^/static/*",req.resource)
-        res2        = FileResponse(string(".",req.resource))
-        res.status  = res2.status
-        res.headers = res2.headers
-        res.data    = res2.data
-    elseif ismatch(r"^/test/?", req.resource)
-        res.status = 200
-        res.data   = view("test", Dict({"something"=>"else"}))
-    else
-        res.status = 404
-        res.data   = "Requested resource not found."
-    end
-    return res
-end
-
-#server = Server(mainhandler)
-#run(server,8000)
 
 app = Morsel.app()
 
@@ -47,11 +30,11 @@ route(app, GET, "/") do req, res
 end
 
 route(app, POST, "/test") do req, res
-    if !haskey(req.data,"repo") || !ismatch(r"^\w+\/\w+$",req.data["repo"])
+    if !haskey(req.state[:data],"repo") || !ismatch(r"^\w+\/\w+$",req.state[:data]["repo"])
         return view("test", Dict({"invalid"=>true}))
     end
     res.headers["Content-Type"] = "text/plain"
-    github_api(string("repos/",req.data["repo"]))
+    github_api(string("repos/",req.state[:data]["repo"]))
 end
 
 # We get signal
