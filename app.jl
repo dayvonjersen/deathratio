2c941716da21d8007d84862ddf4b461ee5360fab
tso <tso@teknik.io>
2015-07-03 04:33:43 -0400

    ayylmao (static files work at this point)

--- app.jl
+++ app.jl
@@ -4,6 +4,8 @@ using HttpServer
 using HTTPClient
 using JSON
 using Mustache
+using Morsel
+using Meddle
 
 function github_api(url::String)
     return JSON.parse(takebuf_string(get(string("https://api.github.com/",url))))
@@ -29,5 +31,28 @@ function mainhandler(req::Request,res::Response)
     return res
 end
 
-server = Server(mainhandler)
-run(server,8000)
+#server = Server(mainhandler)
+#run(server,8000)
+
+app = Morsel.app()
+
+# Static assets handling
+route(app, GET, "/static/<filename::String>") do req, res
+    FileResponse(string("./static/",req.params[:filename]))
+end
+
+# Simple test
+route(app, GET, "/") do req, res
+    view("test", Dict())
+end
+
+route(app, POST, "/test") do req, res
+    if !haskey(req.data,"repo") || !ismatch(r"^\w+\/\w+$",req.data["repo"])
+        return view("test", Dict({"invalid"=>true}))
+    end
+    res.headers["Content-Type"] = "text/plain"
+    github_api(string("repos/",req.data["repo"]))
+end
+
+# We get signal
+start(app, 8000)
--- tpl/test.tpl
+++ tpl/test.tpl
@@ -1,1 +1,8 @@
-<h1>do it or {{ something }}</h1>
+{{#invalid}}
+    <strong>Invalid selection.</strong>
+    <hr>
+{{/invalid}}
+<form action="test" method="post">
+    <input type="text" name="repo" placeholder="Enter a github repo i.e. torvalds/linux">
+    <input type="submit">
+</form>
